b7bc107484f2930d86f56dbc08d26642b8abae36
flasheffects <68129118+flasheffects@users.noreply.github.com>
2023-04-15 11:06:57 +0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // Production
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.use(express.static(__dirname + "/client/build"));
 app.use(express.static(__dirname + "/client/public"));
 
